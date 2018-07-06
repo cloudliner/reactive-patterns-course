c9cc48198f464d32f0981c6e207dba9c3a42a124
cloudliner <cloudliner.jp@gmail.com>
2018-07-06 23:04:18 +0900

    lecture 14

---
+++ src/app/event-bus-experiments/event-bus.ts
@@ -0,0 +1,20 @@
+export interface Observer {
+  notifty(data: any);
+}
+
+interface Subject {
+  registerObserver(obs: Observer);
+  unregisterObserver(obs: Observer);
+  notifyObservers(data: any);
+}
+
+class EventBus implements Subject {
+  private observers: Observer[] = [];
+  registerObserver(obs: Observer) {
+    this.observers.push(obs);
+  }
+  unregisterObserver(obs: Observer) {
+  }
+  notifyObservers(data: any) {
+  }
+}
