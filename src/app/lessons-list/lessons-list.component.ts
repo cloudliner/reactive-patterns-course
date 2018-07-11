6e55962af03c5eb323e5375ebf0c73aac7a047aa
cloudliner <cloudliner.jp@gmail.com>
2018-07-11 10:00:28 +0900

    lecture 24

--- src/app/lessons-list/lessons-list.component.ts
+++ src/app/lessons-list/lessons-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Lesson } from '../shared/model/lesson';
 import {lessonsList$, Observer} from '../event-bus-experiments/app-data';
 
@@ -7,12 +7,11 @@ import {lessonsList$, Observer} from '../event-bus-experiments/app-data';
   templateUrl: './lessons-list.component.html',
   styleUrls: ['./lessons-list.component.css']
 })
-export class LessonsListComponent implements Observer {
+export class LessonsListComponent implements Observer, OnInit {
+
   lessons: Lesson[] = [];
 
-  constructor() {
-    console.log('lesson list component is registered as observer ...');
-
+  ngOnInit(): void {
     lessonsList$.subscribe(this);
   }
 
